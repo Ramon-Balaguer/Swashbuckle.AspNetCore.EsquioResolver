63d899ba822d5be74cf78c0e1088686127e98e0e
Ramon Balaguer <rba.cat@gmail.com>
2020-07-17 20:37:36 +0200

    feat: Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # Swashbuckle.AspNetCore.EsquioResolver
-Conflict solver in actions with features toggles using Esquio
+Conflict solver in actions with features toggles using Esquio.
+
+When using feature toggles you will probably want to use the same endpoint / resource / verb depending on whether a feature toggles is enabled.
+
+```chsarp
+[HttpGet]
+[ActionName("Get")]
+public IEnumerable<Pokemon> Get()
+{
+	return pokemonReader.Read(configuration.GetValue<string>("PokedexPath"));
+}
+
+[FeatureFilter(Name = Flags.PokedexSuper)]
+[HttpGet]
+[ActionName("Get")]
+public IEnumerable<PokemonSuper> GetNew()
+{
+	return pokemonSuperReader.Read(configuration.GetValue<string>("PokedexSuperPath"));
+}
+```
+
+The Swashbuckle documentation does not allow two endpoints / resource / verb to be called the same, because this solution is not supported by OpenApi 3.0.
+
+The solution is to create a conflict solver.
+
+# Requirements
+- .Net Core 3.1.302
+- Nuget: Esquio 
+- Nuget: Swashbuckle 
+
+# Installation
+With package manager:
+```
+Install-Package Swashbuckle.AspNetCore.EsquioResolver
+```
+
+.Net CLI
+```
+dotnet add Swashbuckle.AspNetCore.EsquioResolver
+```
+
+# How to use
+Add these commands in your project's startup.cs ConfigureServices method:
+```chsarp
+services.AddSwaggerGen(configuration =>
+{
+	configuration.ResolveConflictingActionsByFeatureToggles();
+});
+``` 
+
+And in your Configure method:
+```chsarp
+	app.UseResolveConflictingActionsByFeatureToggles();
+```
---
+++ src/Swashbuckle.AspNetCore.EsquioResolver/StartupHelpers.cs
@@ -0,0 +1,78 @@
+﻿using Esquio.Abstractions;
+using Esquio.AspNetCore.Endpoints.Metadata;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Swashbuckle.AspNetCore.EsquioResolver
+{
+    public static class StartupHelpers
+    {
+        internal static IServiceProvider serviceProvider;
+
+        public static void ResolveConflictingActionsByFeatureToggles(this SwaggerGenOptions swaggerGenOptions)
+        {
+            swaggerGenOptions.ResolveConflictingActions(ResolveConflict);
+        }
+
+
+        public static void UseResolveConflictingActionsByFeatureToggles(this IApplicationBuilder applicationServices)
+        {
+            serviceProvider = applicationServices.ApplicationServices;
+        }
+
+        private static ApiDescription ResolveConflict(IEnumerable<ApiDescription> descriptions)
+        {
+            foreach (var apiDescription in descriptions)
+            {
+                if(CheckCandidate(apiDescription))
+                    return apiDescription;
+            }
+            return descriptions.First();
+        }
+
+        private static bool CheckCandidate(ApiDescription apiDescription)
+        {
+            switch (apiDescription.ActionDescriptor)
+            {
+                case ControllerActionDescriptor controllerActionDescriptor:
+                    var filter = controllerActionDescriptor.MethodInfo.GetCustomAttribute<FeatureFilter>();
+                    if (filter != null)
+                    {
+                        return FeatureEnabled(filter.Name);
+                    }
+                    return false;
+                case null:
+                    throw new ArgumentNullException(nameof(apiDescription.ActionDescriptor));
+                default:
+                    return true;
+            }
+        }
+
+        private static bool FeatureEnabled(string featureName)
+        {
+            bool featureEnabled;
+            using (var serviceScope = GetScope())
+            {
+                var featureService = serviceScope.ServiceProvider.GetService<IFeatureService>();
+                featureEnabled = featureService.IsEnabledAsync(featureName).GetAwaiter().GetResult();
+            }
+            return featureEnabled;
+        }
+
+        private static IServiceScope GetScope()
+        {
+            if (serviceProvider == null)
+                throw new Exception("Conflict resolver not configured, please use UseResolveConflictingActionsByFeatureToggles on your startup.cs (Configure method)");
+            return serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope();
+        }
+
+
+    }
+}
